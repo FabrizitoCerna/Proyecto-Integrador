973bc0792c1b6e7ffce83894f0edf092943445de
Fabrizito Cerna <cernafabrizio34@gmail.com>
2026-04-03 15:43:46 -0500

    se agrega el projecto

---
+++ app/src/main/java/com/proyectointegrador/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.proyectointegrador.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
